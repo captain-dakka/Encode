32258fdabaa2be15c87bb9952b7d45560e5bccd3
Oscar Webb <oscarwebb08@gmail.com>
2020-12-26 16:48:22 +1100

    Information Input - Finished first third of info input Fixes #1

--- encode/src/main/java/encode/run.java
+++ encode/src/main/java/encode/run.java
@@ -2,35 +2,68 @@ package encode;
 
 import java.util.Scanner;
 
-public class run{
-    public static void main(String[] args) {
-        //Initate Variables
-        String input;
-        String output; 
-        String lang;
-        Character yn;
-        Boolean cont = false;
-        final Scanner in = new Scanner(System.in);
-        
-        do{
-          //Ask for Language
-          System.out.println("What language do you want to use? NL, EN");
-          lang = in.nextLine();
+public class run {
+	public static void main(String[] args) throws InterruptedException {
+		// Initate Variables
+		String input;
+		String output;
+		String lang = "";
+		String cipher = "";
+		Character yn;
+		Boolean contDo = null;
+		Boolean contDo2 = null;
+		final Scanner in = new Scanner(System.in);
 
-          //Check if they want to use the language they chose
-          System.out.println("Are you sure you wish to use " + lang + " as your language? Y or N");
-          yn = in.nextLine().charAt(0);
+		do {
+			// Lang check
+			do {
+				// Ask for Language
+				System.out.println("What language do you want to use? NL, EN");
+				lang = in.nextLine();
+				lang = lang.toUpperCase();
 
-          if(yn.equals('Y')){
-            cont = true;
-          }else if(yn.equals('N')){
-            System.out.println("Try again.");
-          }else{
-            System.out.println("You have not entered a correct input. Try again");
-          }
-        }while(cont == false);
-        
+				Thread.sleep(300);
 
-        in.close();
-      }
+				if (lang.equals("NL") || lang.equals("EN")) {
+					contDo = false;
+				} else {
+					System.out.println("That is not a language. Please try again.");
+					contDo = true;
+				}
+
+				Thread.sleep(300);
+
+			} while (contDo);
+
+			do {
+				// Check if they want to use the language they chose
+				System.out.println("Are you sure you wish to use " + lang + " as your language? Y or N");
+				yn = in.nextLine().charAt(0);
+				yn = Character.toUpperCase(yn);
+
+				Thread.sleep(300);
+
+				if (yn.equals('Y')) {
+					contDo = false;
+					contDo2 = false;
+				} else if (yn.equals('N')) {
+					System.out.println("Try again.");
+					contDo2 = true;
+				} else {
+					System.out.println("You have not entered a correct input. Try again");
+					contDo = true;
+				}
+
+				Thread.sleep(300);
+
+			} while (contDo);
+
+		} while (contDo2);
+
+		// Taking input
+		// 1. Choose cipher to use
+		System.out.println("Which cipher do you want to use?");
+
+		in.close();
+	}
 }
